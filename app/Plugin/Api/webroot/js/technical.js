63ba275fef2d0888c5383343f915558baa596fb2
CainIsh <mariusz.machuta@gmail.com>
2014-10-17 17:19:05 +0200

    API page

--- app/Plugin/Api/webroot/js/technical.js
+++ app/Plugin/Api/webroot/js/technical.js
@@ -1,7 +1,9 @@
 (function ($) {
-    $('#navbar').css({'width': $('#navbar').width()});
-    $('body').scrollspy({target: '#navbar'});
-    $('#navbar').affix({
+    var $navbar = $('#navbar');
+
+    $navbar.css({'width': $navbar.width()});
+    $('body').scrollspy({target: '#navbar', offset: 80});
+    $navbar.affix({
         offset: {
             top: function () {
                 return (this.top = $('.jumbotron').outerHeight(true))
@@ -9,5 +11,12 @@
                 return (this.bottom = $('footer').outerHeight(true))
             }
         }
-    })
+    });
+    $navbar.find('li > a').click(function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: Math.floor($($(this).attr('href')).offset().top - 40)
+        });
+    });
 }(jQuery));
