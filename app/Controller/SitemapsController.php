47540be6da02ad6683abd0459263e2d9d0797557
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-11-10 01:39:58 +0100

    Sitemap fix

--- app/Controller/SitemapsController.php
+++ app/Controller/SitemapsController.php
@@ -9,7 +9,7 @@ class SitemapsController extends AppController {
 
 	function index() {
 
-		$limit = 50000;
+		$limit = 15000;
 
 		$sitemapindex = array(
 			'xmlns:'  => 'http://www.sitemaps.org/schemas/sitemap/0.9',
