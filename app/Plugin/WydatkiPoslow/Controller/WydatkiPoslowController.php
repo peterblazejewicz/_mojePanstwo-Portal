35ad8704d70446245597a08e7a9bd10a6a43a639
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-11-14 11:07:36 +0100

    Hot news

--- app/Plugin/WydatkiPoslow/Controller/WydatkiPoslowController.php
+++ app/Plugin/WydatkiPoslow/Controller/WydatkiPoslowController.php
@@ -6,7 +6,14 @@ class WydatkiPoslowController extends AppController
 {
     public function index()
     {
+        
         $application = $this->getApplication();
         $this->set('title_for_layout', $application['Application']['name']);
+        
+        $api = $this->API->WydatkiPoslow();
+        
+        $stats = $api->getStats();
+        debug('stats', $stats);
+        
     }
 }
